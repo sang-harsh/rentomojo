31b82258392a4cb15ee4317d8e5b1e157def44ee
sang-harsh <sangharshkhandagale1@gmail.com>
2022-02-14 18:25:57 +0530

    footer alignment

--- src/App.js
+++ src/App.js
@@ -2,22 +2,15 @@ import './App.css';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
-import TableComponent from './components/TableComponent';
-import BlogsPage from './components/BlogsPage';
-import Blog from './components/Blog';
-import UserBlogs from './components/UserBlogs';
+
+import MainBody from './components/MainBody';
 function App() {
   
   return (  
     <Router>
       <div className="App">
         <Navbar/>
-        <Routes>
-        <Route exact path="/" element={<TableComponent/>}></Route>
-        <Route exact path="/blogsPage" element={<BlogsPage/>}></Route>
-        <Route path="/blogsPage/:id" element={<UserBlogs/>}></Route>
-        <Route path="/blog/:id/:postId" element={<Blog/>}></Route>
-        </Routes>
+        <MainBody/>
         <Footer/>
       </div>
     </Router>
---
+++ src/components/MainBody.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Route,Routes} from 'react-router-dom';
+import TableComponent from '../components/TableComponent';
+import BlogsPage from '../components/BlogsPage';
+import Blog from '../components/Blog';
+import UserBlogs from '../components/UserBlogs';
+
+function MainBody() {
+  return (
+    <div className='main-body'>
+      <Routes>
+        <Route exact path="/" element={<TableComponent/>}></Route>
+        <Route exact path="/blogsPage" element={<BlogsPage/>}></Route>
+        <Route path="/blogsPage/:id" element={<UserBlogs/>}></Route>
+        <Route path="/blog/:id/:postId" element={<Blog/>}></Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default MainBody;
