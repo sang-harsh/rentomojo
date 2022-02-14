c7868db1a62c1cb9325f7a14fe5e87c55a781368
sang-harsh <sangharshkhandagale1@gmail.com>
2022-02-14 22:55:43 +0530

    Used Hash Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
-import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
+import {HashRouter as Router,Route,Routes} from 'react-router-dom';
 
 import MainBody from './components/MainBody';
 function App() {
